525b55458b3b2301f79c2c62e458a40ecaaf3042
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-10-21 04:56:35 +0500

    milestone 1 is completed

---
+++ next_js/q2/milestones/1/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center  h-screen">
+      <h1 className="text-4xl font-extrabold ">Hello World</h1></div>
+  )
+}
